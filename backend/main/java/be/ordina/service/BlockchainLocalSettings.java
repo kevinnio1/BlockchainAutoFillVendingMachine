350ca37a42a3bb1af80f7268bc3823453acf7d04
KeLe <kevinleyssens@hotmail.com>
2017-05-16 17:01:07 +0200

    InitPush

--- backend/main/java/be/ordina/service/BlockchainLocalSettings.java
+++ backend/main/java/be/ordina/service/BlockchainLocalSettings.java
@@ -4,7 +4,8 @@ package be.ordina.service;
  * Created by KeLe on 3/05/2017.
  */
 public interface BlockchainLocalSettings {
-    String WALLET_MACHINE = "/home/ubuntu/eth_chain/chain/keystore/UTC--2017-05-16T08-20-35.571839662Z--2dee4f4cc57e448e3311124e11aa4238bb7fdd37";
+    //todo: http://stackoverflow.com/questions/6192661/how-to-reference-a-resource-file-correctly-for-jar-and-debugging
+    String WALLET_MACHINE = "%SystemRoot%/chain/keystore/UTC--2017-05-16T08-20-35.571839662Z--2dee4f4cc57e448e3311124e11aa4238bb7fdd37";
     String VENDING_CONTRACT="0x7D4169B9Fa86c76C90975B82F51e450fa0C1Ad56";
     String VENDING_PASSWORD="azertyui";
 }
