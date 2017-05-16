48de4ac12514365a11d456829f9214efa11dc05a
KeLe <kevinleyssens@hotmail.com>
2017-05-16 16:40:32 +0200

    InitPush

--- backend/main/java/be/ordina/service/BlockchainLocalSettings.java
+++ backend/main/java/be/ordina/service/BlockchainLocalSettings.java
@@ -4,7 +4,7 @@ package be.ordina.service;
  * Created by KeLe on 3/05/2017.
  */
 public interface BlockchainLocalSettings {
-    String WALLET_MACHINE = "C:\\private-ethereum-chain\\chain\\keystore\\UTC--2017-05-02T07-48-09.999839600Z--1c6b88a198a06868d9fab6e54056f04195cfce8c";
-    String VENDING_CONTRACT="0x607C24ff272cb5E99A504b0AA5d26AC7C12fF192";
-    String VENDING_PASSWORD="cola";
+    String WALLET_MACHINE = "/home/ubuntu/eth_chain/chain/keystore/UTC--2017-05-16T08-20-35.571839662Z--2dee4f4cc57e448e3311124e11aa4238bb7fdd37";
+    String VENDING_CONTRACT="0x7D4169B9Fa86c76C90975B82F51e450fa0C1Ad56";
+    String VENDING_PASSWORD="azertyui";
 }
